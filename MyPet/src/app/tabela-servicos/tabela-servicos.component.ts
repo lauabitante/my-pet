ff3620d112f936245f7346b39a25317ad99ca6c6
LucasGuasselli <lucaszack265@gmail.com>
2017-10-14 11:17:28 -0300

    Criação do component tabela-servicos.

---
+++ MyPet/src/app/tabela-servicos/tabela-servicos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-servicos',
+  templateUrl: './tabela-servicos.component.html',
+  styleUrls: ['./tabela-servicos.component.css']
+})
+export class TabelaServicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
