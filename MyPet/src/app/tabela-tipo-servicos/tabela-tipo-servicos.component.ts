f2390e4d28a765eea279d844b1008cf75e924ac5
Laura Abitante <laura.abitante@gmail.com>
2017-10-21 23:24:07 -0200

    Adicionado logo principal

--- MyPet/src/app/tabela-tipo-servicos/tabela-tipo-servicos.component.ts
+++ MyPet/src/app/tabela-tipo-servicos/tabela-tipo-servicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { TipoServico } from '../tipo-servico';
 import { CrudTipoServicosService } from '../crud-tipo-servicos.service';
 
