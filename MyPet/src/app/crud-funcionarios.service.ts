e99c05c244aec0b493c66dd1f26f75e44c40f132
LucasGuasselli <lucaszack265@gmail.com>
2017-10-23 20:41:20 -0200

    Ajuste layout

--- MyPet/src/app/crud-funcionarios.service.ts
+++ MyPet/src/app/crud-funcionarios.service.ts
@@ -25,14 +25,14 @@ export class CrudFuncionariosService {
 
   autoIncrement: number = 2;
   constructor() {
-        if(!localStorage.getItem("funcionarios")){
-                localStorage.setItem("funcionarios",JSON.stringify(this.funcionarios))
-        }
+        // if(!localStorage.getItem("funcionarios")){
+        //         localStorage.setItem("funcionarios",JSON.stringify(this.funcionarios))
+        // }
     }
     getFuncionarios() {
-         if(localStorage.getItem("funcionarios")){
-                this.funcionarios = JSON.parse(localStorage.getItem("funcionarios"));
-         }
+        //  if(localStorage.getItem("funcionarios")){
+        //         this.funcionarios = JSON.parse(localStorage.getItem("funcionarios"));
+        //  }
         return this.funcionarios;
     }
 
@@ -43,13 +43,13 @@ export class CrudFuncionariosService {
     adicionarFuncionario(funcionario: Funcionario) {
         funcionario.codigo = this.autoIncrement++;
         this.funcionarios.push(funcionario); 
-        localStorage.setItem("funcionarios",JSON.stringify(this.funcionarios)); 
+        // localStorage.setItem("funcionarios",JSON.stringify(this.funcionarios)); 
     }
 
     removerFuncionario(funcionario: Funcionario) {
         let indice = this.funcionarios.indexOf(funcionario, 0);
         if (indice >- 1) {
-            localStorage.removeItem('funcionarios');
+            // localStorage.removeItem('funcionarios');
             this.funcionarios.splice(indice, 1);
         }
     }
